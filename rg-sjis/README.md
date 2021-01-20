dee5380e9e2bff4e8a7725fb5d98b08988f15583
komiyamma <akitsugu-komiyama@docomo.ne.jp>
2021-01-20 12:26:32 +0900

    まずは最低動作はする版

--- rg-sjis/README.md
+++ rg-sjis/README.md
@@ -1,7 +1,12 @@
 # rg-sjis
 
-sss
+This is the for your extension "rg-sjis".
 
+## Features
+
+Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+
+For example if there is an image subfolder under your extension project workspace:
 
 ## Requirements
 
@@ -9,14 +14,6 @@ If you have any requirements or dependencies, add a section describing those and
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
 
 ## Known Issues
 
@@ -37,4 +34,5 @@ Fixed issue #.
 ### 1.1.0
 
 Added features X, Y, and Z.
+
 
--- rg-sjis/src/extension.ts
+++ rg-sjis/src/extension.ts
@@ -2,12 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 var exec = require('child_process').exec;
+const path = require('path');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	var COMMAND = "C:/usr/hidemaru/HmBaloonToast/HmBaloonToast.exe";
-	exec(COMMAND, function(error:any, stdout:any, stderr:any) {
+	var command = path.dirname(__filename) + "/rg_sjis.exe";
+	exec(command, function(error:any, stdout:any, stderr:any) {
 		// シェル上でコマンドを実行できなかった場合のエラー処理
 		if (error !== null) {
 		  console.log('exec error: ' + error);
@@ -19,28 +20,24 @@ export function activate(context: vscode.ExtensionContext) {
 	  });
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rg-sjis" is now active!');
+	vscode.window.showInformationMessage('Congratulations, your extension "rg-sjis" is now Active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rg-sjis.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from rg-sjis!');
-	});
+	let disposable = vscode.commands.registerCommand('rg-sjis.Install', () => {});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+	// abc
 	console.log('Congratulations, your extension "rg-sjis" is now de-active!');
-	vscode.window.showInformationMessage('Congratulations, your extension "rg-sjis" is now de-active!');
+	vscode.window.showInformationMessage('Congratulations, your extension "rg-sjis" is now De-Active!');
 
-	var COMMAND = "C:/usr/hidemaru/HmBaloonToast/HmBaloonToast.exe";
-	exec(COMMAND, function(error:any, stdout:any, stderr:any) {
+	var command = path.dirname(__filename) + "/rg_sjis.exe --mode-uninstall";
+	exec(command, function(error:any, stdout:any, stderr:any) {
 		// シェル上でコマンドを実行できなかった場合のエラー処理
 		if (error !== null) {
 		  console.log('exec error: ' + error);
