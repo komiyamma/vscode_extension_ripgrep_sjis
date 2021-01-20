79d0269edd4abeb3fc2f6e2266dcebf931488208
komiyamma <akitsugu-komiyama@docomo.ne.jp>
2021-01-20 20:00:52 +0900

    デバッグ表示が残っていたためカット

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@ const path = require('path');
 var process = require('process');
 
 export function activate(context: vscode.ExtensionContext) {
-	vscode.window.showInformationMessage(process.execPath);
 	var command = path.dirname(__filename) + "/rg_sjis.exe --mode-install"+ " " + "\"" +process.execPath + "\"";
 	exec(command, function(error:any, stdout:any, stderr:any) {
 		// シェル上でコマンドを実行できなかった場合のエラー処理
