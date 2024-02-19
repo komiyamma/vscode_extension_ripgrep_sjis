24f3587b4ae10ab43587572309761535a3f30789
komiyamma <komiyama0629@gmail.com>
2024-02-19 22:31:56 +0900

    実行されないなぁ。これ...

--- dist/extensionUninstall.js
+++ dist/extensionUninstall.js
@@ -2,20 +2,20 @@ const fs = require('fs');
 
 try {
 	var rg_dir = require("./rg_sjis.json");
-    var dir = rg_dir.Path;
+	var dir = rg_dir.Path;
 
-    const srcPath = dir + '/rg_utf8.exe';
-    const dstPath = dir + '/rg.exe';
-    
+	const srcPath = dir + '/rg_utf8.exe';
+	const dstPath = dir + '/rg.exe';
+
 	// ファイルを上書きコピーする
 	fs.copyFile(srcPath, dstPath, (err) => {
-	  if (err) {
-	    console.error(err);
-	    return;
-	  }
+		if (err) {
+			console.error(err);
+			return;
+		}
 
-	  console.log('ファイルを上書きコピーしました');
+		console.log('ファイルを上書きコピーしました');
 	});
-} catch(e) {
-    console.log("error" + e);
+} catch (e) {
+	console.log("error" + e);
 }
